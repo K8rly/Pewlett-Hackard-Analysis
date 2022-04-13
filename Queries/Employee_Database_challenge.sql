e90339c6b5d010d2d2a8b87287f46f201435dc33
Katterli <kwrikeman07@jcu.edu>
2022-04-13 09:34:00 -0400

    deliverable 14

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,36 @@
+SELECT e.emp_no, e.first_name, e.last_name
+FROM employees as e;
+
+SELECT t.title, t.from_date, t.to_date
+FROM titles as t;
+
+SELECT e.emp_no, 
+		e.first_name, 
+		e.last_name,
+		t.title, 
+		t.from_date, 
+		t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+--Number of employees retiring by job title
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count desc;
