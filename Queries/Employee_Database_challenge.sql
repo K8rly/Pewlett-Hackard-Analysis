1e078941eb31270c09e7c0a59d2540bdf4e91375
Katterli <kwrikeman07@jcu.edu>
2022-04-14 12:56:10 -0400

    final commit

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -35,27 +35,7 @@ FROM unique_titles
 GROUP BY title
 ORDER BY count desc;
 
---Mentorship (gives "Staff" twice)
-SELECT DISTINCT ON(t.emp_no) e.emp_no,
-		e.first_name,
-		e.last_name, 
-		e.birth_date,
-		de.from_date, 
-		de.to_date,
-		t.title
-		--DISTINCT ON(emp_no)
---INTO Mentorship
-FROM employees as e
-	INNER JOIN dept_emp as de
-		ON (e.emp_no = de.emp_no)
-	INNER JOIN titles as t
-		ON (e.emp_no = t.emp_no)
-WHERE (de.to_date = '9999-01-01') 
-	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-ORDER BY t.emp_no asc;
-
--- Mentorship gives "Senior Staff" twice
-
+--Number of employees eligible for mentorship
 SELECT DISTINCT ON(e.emp_no) e.emp_no,
 		e.first_name,
 		e.last_name, 
